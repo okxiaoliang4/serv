288213bf7dd4bddb7fdc734c9da755ff5f31e2bf
Jelf <okxiaoliang4@gmail.com>
2022-09-30 15:36:48 +0800

    feat: dir args

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,26 @@
+use clap::{arg, command, Parser};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+#[clap(version = "1.0", author = "Jelf")]
+struct Opts {
+    #[arg(short, long)]
+    dir: String,
+}
+
 #[tokio::main]
 async fn main() {
-    warp::serve(warp::fs::dir("."))
+    let currentDir = std::env::current_dir()
+        .unwrap()
+        .to_str()
+        .unwrap()
+        .to_string();
+
+    // TODO: default to current dir
+    let opts = Opts::parse();
+    println!("opts: {:?}", opts);
+
+    warp::serve(warp::fs::dir(opts.dir))
         .run(([127, 0, 0, 1], 3030))
         .await;
 }
