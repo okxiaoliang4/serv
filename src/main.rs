7ff7b17601997723a3e61566e7b64b081135eff7
Jelf <okxiaoliang4@gmail.com>
2022-10-30 13:37:02 +0800

    chore: update

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,32 @@
 use clap::{arg, command, Parser};
+use warp::Filter;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 #[clap(version = "1.0", author = "Jelf")]
 struct Opts {
     #[arg(short, long)]
-    dir: String,
+    dir: Option<String>,
 }
 
 #[tokio::main]
 async fn main() {
-    let currentDir = std::env::current_dir()
-        .unwrap()
-        .to_str()
-        .unwrap()
-        .to_string();
-
-    // TODO: default to current dir
     let opts = Opts::parse();
+
     println!("opts: {:?}", opts);
 
-    warp::serve(warp::fs::dir(opts.dir))
-        .run(([127, 0, 0, 1], 3030))
-        .await;
+    let work_dir = match opts.dir {
+        Some(s) => s,
+        None => std::env::current_dir()
+            .unwrap()
+            .to_str()
+            .unwrap()
+            .to_string(),
+    };
+
+    dbg!(&work_dir);
+
+    let route = warp::get().and(warp::fs::dir(work_dir));
+
+    warp::serve(route).run(([127, 0, 0, 1], 3030)).await;
 }
